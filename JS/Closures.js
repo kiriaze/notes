9a115ca1a69c9e25bfdd1c978767f79a91697a6c
Zach Carroll <carroll.zach@gmail.com>
2019-01-16 09:32:40 -0800

    Update Closures.js

--- JS/Closures.js
+++ JS/Closures.js
@@ -1,10 +1,11 @@
-/** 
-CLOSURES IN JS 
-
-Closures are the idea that function declarations have access
-to the variables in the execution context's scope, even if that
-scope has been removed. This is useful for maintaining private access
-to values. 
+/**
+* Closures
+*
+* Every function in JS creates a closure. A closure is an inner function
+* (within an outer/parent function or global scope) that captures it's
+* parent's scope and retains it, even after the parent has been executed.
+* The result is a function that has access to variables defined in a parent
+* that can no longer be accessed directly.
 */
 
 function createCounter() {
